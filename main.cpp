4703def70f8f96fdb898adaa307db265032004fd
Timothy Graupmann <tgraupmann@gmail.com>
2023-11-13 11:27:17 -0800

    Added sample callback

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "libplatform/libplatform.h"
+#include "v8.h"
 #include "v8-context.h"
 #include "v8-initialization.h"
 #include "v8-isolate.h"
@@ -16,6 +17,32 @@
 #pragma comment(lib, "v8_libplatform.dll.lib")
 
 using namespace std;
+
+// Define a simple C++ function to be called from JavaScript
+void MyCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
+    // Check the number of arguments passed
+    if (args.Length() < 1) {
+        args.GetIsolate()->ThrowException(
+            v8::String::NewFromUtf8(args.GetIsolate(), "Invalid number of arguments")
+            .ToLocalChecked());
+        return;
+    }
+
+    // Check if the first argument is a string
+    if (!args[0]->IsString()) {
+        args.GetIsolate()->ThrowException(
+            v8::String::NewFromUtf8(args.GetIsolate(), "Argument must be a string")
+            .ToLocalChecked());
+        return;
+    }
+
+    // Convert the first argument to a string
+    v8::Local<v8::String> str = args[0].As<v8::String>();
+    v8::String::Utf8Value utf8(args.GetIsolate(), str);
+
+    // Print the string to the console
+    printf("Callback invoked with argument: %s\n", *utf8);
+}
 
 int main(int argc, char* argv[]) {
     // Initialize V8.
@@ -37,10 +64,32 @@ int main(int argc, char* argv[]) {
         v8::Local<v8::Context> context = v8::Context::New(isolate);
         // Enter the context for compiling and running the hello world script.
         v8::Context::Scope context_scope(context);
+
+#pragma region Create C++ Callback
+
+        // Expose the C++ function to JavaScript
+        v8::Local<v8::Object> global = context->Global();
+        v8::Local<v8::ObjectTemplate> global_template = v8::ObjectTemplate::New(isolate);
+        global_template->Set(
+            isolate,
+            "myCallback",
+            v8::FunctionTemplate::New(isolate, MyCallback));
+
+        // Create an instance of the template and add it to the global object
+        v8::Local<v8::Object> global_instance = global_template->NewInstance(context).ToLocalChecked();
+        global->Set(
+            context,
+            v8::String::NewFromUtf8(isolate, "MyNamespace").ToLocalChecked(),
+            global_instance);
+
+
+#pragma endregion Create C++ Callback
+
+
         {
             // Create a string containing the JavaScript source code.
             v8::Local<v8::String> source =
-                v8::String::NewFromUtf8Literal(isolate, "'Hello, World!';");
+                v8::String::NewFromUtf8Literal(isolate, "MyNamespace.myCallback('hello'); 'Hello, World!';");
             // Compile the source code.
             v8::Local<v8::Script> script =
                 v8::Script::Compile(context, source).ToLocalChecked();
